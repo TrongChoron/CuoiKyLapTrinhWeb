bd03e89719836c48d6ecf5e38ab6aa45a0701717
TrongChoron <tronglagi111222@gmail.com>
2021-11-21 11:11:53 +0700

    Create Function to get data from database

--- src/main/java/com/myshop/dao/GenericDao.java
+++ src/main/java/com/myshop/dao/GenericDao.java
@@ -5,10 +5,18 @@
  */
 package com.myshop.dao;
 
+import java.io.Serializable;
+import java.util.List;
+
 /**
  *
  * @author asus
  */
-public interface GenericDao {
-    
+public interface GenericDao<ID extends Serializable, T> {
+    List<T> findAll();
+    T update(T entity);
+    void save(T entity);
+    T findById(ID id);
+    Object[] findByProperty(String property, Object value,String sortExpression, String sortDirection);
+    Integer delete(List<ID> ids);
 }
